6e0ba89fc155ac2e4773c6b2ff5a91c5459cbe95
LAPTOP-HMDMNQJO\dinya <dinyarian.farzin@gmail.com>
2019-12-22 15:52:38 -0800

    Hello World

--- Week1/HelloWorld/src/main/java/com/myclu/helloworld/Main.java
+++ Week1/HelloWorld/src/main/java/com/myclu/helloworld/Main.java
@@ -10,5 +10,7 @@ package com.myclu.helloworld;
  * @author dinya
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+    }
 }
