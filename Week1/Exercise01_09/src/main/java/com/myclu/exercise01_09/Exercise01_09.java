e0b9872cc79226aff8f54d3e2624ba7d63653168
LAPTOP-HMDMNQJO\dinya <dinyarian.farzin@gmail.com>
2019-12-22 18:45:55 -0800

    Excersie01_09

---
+++ Week1/Exercise01_09/src/main/java/com/myclu/exercise01_09/Exercise01_09.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myclu.exercise01_09;
+
+/**
+ * <p>
+ *  (Area and perimeter of a rectangle) Write a program that displays the area and
+ *  perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following
+ *  formula:
+ *          area = width * height
+ * 
+ * @author      Farzin Dinyarian
+ * @version     1.0
+ * @since       12/22/2019
+ */
+public class Exercise01_09 {
+    /**
+    * main function to print area.
+    * @param args 
+    * @see       
+    * @since     12/22/2019
+    */  
+    public static void main(String[] args) {
+    // Display area
+    System.out.println(4.5 * 7.9);
+
+    // Display perimeter
+    System.out.println(2 * (4.5 + 7.9));
+  }
+}
