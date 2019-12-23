7d0a82b2c7d7fa1a9e9064c9c3004c588025797b
LAPTOP-HMDMNQJO\dinya <dinyarian.farzin@gmail.com>
2019-12-22 18:22:11 -0800

    Excersie01_03

---
+++ Week1/Exercise01_03/src/main/java/com/myclu/exercise01_03/Exercise01_03.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myclu.exercise01_03;
+
+/**
+ * <p>
+ * (Display a pattern) Write a program that displays the following pattern:
+            J   A V     V  A
+            J  A A V   V  A A
+        J   J AAAAA V V  AAAAA
+         J J  A   A  V   A   A
+ * 
+ * @author      Farzin Dinyarian
+ * @version     %I%, %G%
+ * @since       1.0
+ */
+public class Exercise01_03 {
+    /**
+     * main function to print JAVA String.
+     * @param args 
+     * @see       
+     * @since     1.0
+     */  
+  public static void main(String[] args) {
+    System.out.println("    J     A     V     V    A     ");  
+    System.out.println("    J    A A     V   V    A A    ");   
+    System.out.println("J   J   AAAAA     V V    AAAAA   ");
+    System.out.println(" J J   A     A     V    A     A  ");
+  }
+}
