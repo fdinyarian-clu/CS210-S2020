187609875c50ea804c58480064f957e7f44671fd
LAPTOP-HMDMNQJO\dinya <dinyarian.farzin@gmail.com>
2019-12-23 17:35:14 -0800

    Excersie02_18

---
+++ Week2/Exercise02_18/src/main/java/com/myclu/exercise02_18/Exercise02_18.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myclu.exercise02_18;
+
+/**
+ * @author      Farzin Dinyarian
+ * @version     1.0
+ * @since       12/22/2019
+ */
+public class Exercise02_18 {
+      // Main method
+  public static void main(String[] args) {
+    System.out.println("a      b      pow(a, b)");
+    System.out.println("1      2      " + (int)Math.pow(1, 2));
+    System.out.println("2      3      " + (int)Math.pow(2, 3));
+    System.out.println("3      4      " + (int)Math.pow(3, 4));
+    System.out.println("4      5      " + (int)Math.pow(4, 5));
+    System.out.println("5      6      " + (int)Math.pow(5, 6));
+  }    
+}
